d38d226cb734c7cdf5ca0d5d369ecf4887bc9a25
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2019-11-07 16:50:49 +0530

    added idea module crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IdeaModule } from './idea/idea.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [IdeaModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
